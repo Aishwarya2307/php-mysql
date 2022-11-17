35a0990e1fa66fa26b67c849902387ab2db24d72
Aishwarya2307 <100131940+Aishwarya2307@users.noreply.github.com>
2022-11-17 23:31:00 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'database-1.cunmyjc2dhmm.ap-south-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'Aishwarya');
+define('DB_PASSWORD', 'password');
 define('DB_NAME', 'employees_db');
  
 /* Attempt to connect to MySQL database */
